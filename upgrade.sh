33c70b6ecfa6af273d33c9b53d641b2635ce30f2
80017 <arpitap@officebeacon.com>
2018-02-06 16:41:36 +0530

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -46,4 +46,4 @@ curl -u ""$RANCHER_USER":"$RANCHER_PASS"" \
 -H 'Content-Type: application/json' \
 -d '{
      "inServiceStrategy":{"launchConfig": {"imageUuid":"docker:'$USERNAME'/payment_service_flowz:'$TAG'","kind": "container","labels":{"io.rancher.container.pull_image": "always","io.rancher.scheduler.affinity:host_label": "machine=cluster-flowz"},"ports": ["3031:3031/tcp"],"healthCheck": {"type": "instanceHealthCheck","healthyThreshold": 2,"initializingTimeout": 60000,"interval": 2000,"name": null,"port": 3031,"recreateOnQuorumStrategyConfig": {"type": "recreateOnQuorumStrategyConfig","quorum": 1},"reinitializingTimeout": 60000,"responseTimeout": 60000,"strategy": "recreateOnQuorum","unhealthyThreshold": 3},"networkMode": "managed"}},"toServiceStrategy":null}' \
-'http://rancher.flowz.com:8080/v2-beta/projects/1a29/services/$SERVICE_ID?action=upgrade'
+'http://rancher.flowz.com:8080/v2-beta/projects/$ENV_ID/services/$SERVICE_ID?action=upgrade'
